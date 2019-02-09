4a5b488ceee0cb1a6491660c965fd7fa16430665
Thirumuruga <thirumurugan3131@gmail.com>
2019-02-09 12:45:12 +0530

    test configured

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import styles from "../style/App.module.scss";
 
 class App extends Component {
   render() {
-    return <div>Under construction</div>;
+    return <div className={styles.container}>hello world</div>;
   }
 }
 
